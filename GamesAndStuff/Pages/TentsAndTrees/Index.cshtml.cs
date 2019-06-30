256b79510ea8f7aa6f8f5992e2537eb00594b74c
Khoa Nguyen <khoanguyen0497@gmail.com>
2019-07-01 03:05:53 +1200

    init

--- GamesAndStuff/Pages/TentsAndTrees/Index.cshtml.cs
+++ GamesAndStuff/Pages/TentsAndTrees/Index.cshtml.cs
@@ -10,30 +10,95 @@ namespace GamesAndStuff.Pages.TentsAndTrees
 {
     public class IndexModel : PageModel
     {
-
-        public class GridCell
+        public enum Status
         {
-            bool isTent;    
+            None,
+            Grass,
+            Tree,
+            Tent
+        };
+
+        public class GridCell : ICloneable
+        {
+            public Status Status { get; set; }
+            public int Row { get; set; }
+            public int Column { get; set; }
+            public decimal Probability { get; set; }
+
+            public object Clone()
+            {
+                return new GridCell
+                {
+                    Row = Row,
+                    Column = Column,
+                    Status = Status,
+                    Probability = Probability
+                };
+            }
+        }
+
+        public class Constraint : ICloneable
+        {
+            public int Row { get; set; }
+            public int Column { get; set; }
+            public int NumberOfTents { get; set; }
+
+            public object Clone()
+            {
+                return new Constraint
+                {
+                    Row = Row,
+                    Column = Column,
+                    NumberOfTents = NumberOfTents
+                };
+            }
         }
 
         [BindProperty]
-        public List<List<GridCell>> Grid { get; set;}
+        public List<GridCell> Grid { get; set; }
 
         [BindProperty]
-        public int Rows { get; set; }
+        public List<Constraint> Constraints { get; set; }
 
-        [BindProperty]
-        public int Columns { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int Rows { get; set; } = 5;
+
+        [BindProperty(SupportsGet = true)]
+        public int Columns { get; set; } = 5;
 
         public List<SelectListItem> numbers;
 
-        public void OnGet(int r = 4, int c = 4)
+        public void OnGet()
         {
-            Rows = r;
-            Columns = c;
+            InitiateNumberList();
+            Grid = new List<GridCell>();
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    Grid.Add(new GridCell
+                    {
+                        Row = i,
+                        Column = j,
+                        Status = Status.None
+                    });
+                }
+            }
 
+
+        }
+
+        public IActionResult OnPost()
+        {
+            Grid = Solve(Grid, Constraints, 0);
+            InitiateNumberList();
+            return Page();
+        }
+
+        public void InitiateNumberList()
+        {
             numbers = new List<SelectListItem>();
-            for (int i = 4; i<8; i++)
+            for (int i = 4; i < 10; i++)
             {
                 numbers.Add(new SelectListItem
                 {
@@ -43,9 +108,238 @@ namespace GamesAndStuff.Pages.TentsAndTrees
             }
         }
 
-        public IActionResult OnPostGenerateGrid()
+        public List<GridCell> Solve(List<GridCell> grid, List<Constraint> constraints, int iteration)
         {
-            return Page();
+            if (!IsValid(grid, constraints))
+            {
+                return null;
+            }
+
+            PlantGrassOnZeroConstraints(grid, constraints); // Dont need to clone, cant go wrong here (hopefully)
+            PlantGrassOnCellsWithNoAdjacentTree(grid);
+            UpdateProbability(grid, constraints);
+
+            //Step 1: Clone the all the lists
+            List<GridCell> clonedGrid = grid.Clone();
+            List<Constraint> clonedConstraints = constraints.Clone();
+
+            if (IsValid(clonedGrid, clonedConstraints) && !clonedGrid.Any(c => c.Status == Status.None))
+            {
+                return clonedGrid;
+            }
+            //Step 2: Find the slot with highest probability to have a tent
+            GridCell cell = clonedGrid.Where(c => c.Status == Status.None).OrderBy(c => c.Probability).Last();
+
+            //Step 3: Put a tent on it
+            AssignTent(cell, clonedGrid, clonedConstraints);
+
+            //If its invalid, try planting grass on it then keep going using the original grid
+            if (!IsValid(clonedGrid, clonedConstraints))
+            {
+                //Find the original cell
+                GridCell originalCell = grid.First(c => c.Row == cell.Row && c.Column == cell.Column);
+                originalCell.Status = Status.Grass;
+                return Solve(grid, constraints, iteration);
+            }
+
+            //Else, if valid, check if its complete. Return if it is, else, keep going using the cloned list
+            if (!clonedGrid.Any(c => c.Status == Status.None))
+            {
+                return clonedGrid;
+            }
+
+            var t = Solve(clonedGrid, clonedConstraints, iteration += 1);
+            //If fails, current tent does not work
+            if (t == null)
+            {
+                //Find the original cell
+                GridCell originalCell = grid.First(c => c.Row == cell.Row && c.Column == cell.Column);
+                originalCell.Status = Status.Grass;
+                return Solve(grid, constraints, iteration);
+            }
+
+            return t;
+        }
+
+        public void UpdateProbability(List<GridCell> grid, List<Constraint> constraints)
+        {
+            for (int index = 0; index < grid.Count(); index++)
+            {
+                decimal rowProbability = 0;
+                decimal columnProbability = 0;
+
+                Constraint rowConstraint = constraints.FirstOrDefault(c => c.Row == grid[index].Row);
+                Constraint columnConstraint = constraints.FirstOrDefault(c => c.Column == grid[index].Column);
+
+                int numberOfEmptySlotsInRow = grid.Where(c => c.Row == grid[index].Row && c.Status == Status.None).Count();
+                int numberOfEmptySlotsInColumn = grid.Where(c => c.Column == grid[index].Column && c.Status == Status.None).Count();
+                if (grid[index].Status == Status.None)
+                {
+                    if (rowConstraint.NumberOfTents == 0)
+                    {
+                        rowProbability = 0;
+                    }
+                    else
+                    {
+                        rowProbability = ((decimal)PermutationsAndCombinations.Combination(numberOfEmptySlotsInRow - 1, rowConstraint.NumberOfTents - 1)) / PermutationsAndCombinations.Combination(numberOfEmptySlotsInRow, rowConstraint.NumberOfTents);
+                    }
+
+                    if (columnConstraint.NumberOfTents == 0)
+                    {
+                        columnProbability = 0;
+                    }
+                    else
+                    {
+                        columnProbability = ((decimal)PermutationsAndCombinations.Combination(numberOfEmptySlotsInColumn - 1, columnConstraint.NumberOfTents - 1)) / PermutationsAndCombinations.Combination(numberOfEmptySlotsInColumn, columnConstraint.NumberOfTents);
+                    }
+                }
+
+                if (rowProbability == 0 || columnProbability == 0)
+                {
+                    grid[index].Probability = 0;
+                }
+                else if (rowProbability == 1 || columnProbability == 1)
+                {
+                    grid[index].Probability = 1;
+                }
+                else
+                {
+                    grid[index].Probability = rowProbability * columnProbability;
+                }
+            }
+        }
+
+        //Plant grass if there are rows/columns with 0 tents
+        public void PlantGrassOnZeroConstraints(List<GridCell> grid, List<Constraint> constraints)
+        {
+            foreach (Constraint constraint in constraints)
+            {
+                if (constraint.NumberOfTents == 0)
+                {
+                    foreach (GridCell cell in grid.Where(c => (c.Row == constraint.Row || c.Column == constraint.Column) && c.Status == Status.None))
+                    {
+                        cell.Status = Status.Grass;
+                    }
+                }
+            }
+        }
+
+        //Some cells cannot have tents because no trees around
+        public void PlantGrassOnCellsWithNoAdjacentTree(List<GridCell> grid)
+        {
+            for (int i = 0; i < grid.Count(); i++)
+            {
+                if (grid[i].Status != Status.None)
+                {
+                    continue;
+                }
+
+                if (GetAdjacentCells(grid[i], grid, false).FirstOrDefault(c => c.Status == Status.Tree) == null)
+                {
+                    grid[i].Status = Status.Grass;
+                }
+            }
+        }
+
+        public List<GridCell> GetAdjacentCells(GridCell cell, List<GridCell> grid, bool diagonal)
+        {
+            int rowIndex = cell.Row;
+            int columnIndex = cell.Column;
+
+            var adjacentCells = grid.Where(c => (Math.Abs(c.Row - rowIndex) <= 1 && Math.Abs(c.Column - columnIndex) <= 1) && c != cell);
+            if (!diagonal)
+            {
+                adjacentCells = adjacentCells.Where(c => !(Math.Abs(c.Row - rowIndex) == 1 && Math.Abs(c.Column - columnIndex) == 1));
+            }
+
+            return adjacentCells.ToList();
+        }
+
+        public void AssignTent(GridCell cell, List<GridCell> grid, List<Constraint> constraints)
+        {
+            //Assign tent
+            cell.Status = Status.Tent;
+
+            //Update the associated constraints
+            foreach (Constraint constraint in constraints.Where(c => c.Row == cell.Row || c.Column == cell.Column))
+            {
+                constraint.NumberOfTents -= 1;
+            }
+
+            //Update grid
+            PlantGrassOnZeroConstraints(grid, constraints);
+            PlantGrassOnCellsWithNoAdjacentTree(grid);
+            PlantGrassAroundTents(grid);
+            UpdateProbability(grid, constraints);
+        }
+
+        public void PlantGrassAroundTents(List<GridCell> grid)
+        {
+            foreach (GridCell cell in grid.Where(c => c.Status == Status.None && GetAdjacentCells(c, grid, true).Any(c2 => c2.Status == Status.Tent)))
+            {
+                cell.Status = Status.Grass;
+            }
+        }
+
+        public bool IsValid(List<GridCell> grid, List<Constraint> constraints)
+        {
+            //Check if constraints are still valid
+            foreach (Constraint constraint in constraints)
+            {
+                int rCells = grid.Where(c => c.Row == constraint.Row && c.Status == Status.None).Count();
+                int cCells = grid.Where(c => c.Column == constraint.Column && c.Status == Status.None).Count();
+
+                if (rCells + cCells - constraint.NumberOfTents < 0)
+                {
+                    return false;
+                }
+            }
+
+            //Check if tents are adjacent to each other         
+            if (grid.Where(c => c.Status == Status.Tent).Any(c => GetAdjacentCells(c, grid, false).Any(c2 => c2.Status == Status.Tent)))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    public static class PermutationsAndCombinations
+    {
+        public static long Combination(int n, int r)
+        {
+            // naive: return Factorial(n) / (Factorial(r) * Factorial(n - r));
+            return Permutation(n, r) / Factorial(r);
+        }
+
+        public static long Permutation(int n, int r)
+        {
+            // naive: return Factorial(n) / Factorial(n - r);
+            return FactorialDivision(n, n - r);
+        }
+
+        private static long FactorialDivision(int topFactorial, int divisorFactorial)
+        {
+            long result = 1;
+            for (int i = topFactorial; i > divisorFactorial; i--)
+                result *= i;
+            return result;
+        }
+
+        private static long Factorial(int i)
+        {
+            if (i <= 1)
+                return 1;
+            return i * Factorial(i - 1);
+        }
+    }
+
+    static class Extensions
+    {
+        public static List<T> Clone<T>(this IList<T> listToClone) where T : ICloneable
+        {
+            return listToClone.Select(item => (T)item.Clone()).ToList();
         }
     }
 }
